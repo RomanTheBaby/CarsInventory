29dcf7c1c44007887cb40df7d1f5f3be2c35575a
RomanTheBaby <mailofrom@gmail.com>
2025-01-04 17:35:12 -0700

    Initial Commit

---
+++ CarsInventory/CarsInventoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarsInventoryApp.swift
+//  CarsInventory
+//
+//  Created by Roman on 2025-01-04.
+//
+
+import SwiftUI
+
+@main
+struct CarsInventoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
